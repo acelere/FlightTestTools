e137abc2226bf2a3762c0ba125440ed2b6f5e30b
acelere <acelere@gmail.com>
2018-10-01 16:40:09 -0400

    VN excess values are now in a list

--- datamerger/data_merger_VectorNav_Graphtec_autopeaks.py
+++ datamerger/data_merger_VectorNav_Graphtec_autopeaks.py
@@ -28,38 +28,35 @@ class FTISyncApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         
         tk.Tk.__init__(self, *args, **kwargs)
-
         tk.Tk.wm_title(self, "Graphtec/INS Data Merge")
-        
         
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand = True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        
+
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo):
-
-            frame = F(container, self)
-
-            self.frames[F] = frame
-
-            frame.grid(row=0, column=0, sticky="nsew")
+        frame = StartPage(container, self)
+        self.frames[StartPage] = frame
+        frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
+        
+
 
     def show_frame(self, cont):
 
         frame = self.frames[cont]
         frame.tkraise()
         
-    
 
 
 class StartPage(tk.Frame):
-
-
+    ''' This class is the place holder for the main page interface
+    '''
     
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
@@ -86,13 +83,15 @@ class StartPage(tk.Frame):
         button5.pack()
 
         self.saveOK = False
-
-
         self.figInit = False
         
 
 
     def updatePlot(self, gr_plt_data, vn_plt_data):
+        '''data in:
+            gr_plt_data: pandas dataframe that contains the graphtec FILTERED data for the graph
+            vn_plt_data: same thing for the vectornav
+        '''
 
         if self.figInit == True:
             self.a.clear()
@@ -120,8 +119,11 @@ class StartPage(tk.Frame):
 
 
 
-
     def getGraphtecFile(self):
+        '''
+        read the Graphtec csv file
+        get rid of lines as listed in skiprows below
+        '''
 
         filename = tk.filedialog.askopenfilename(filetypes=[('CSV','*.csv')], title='Select GRAPHTEC file')
 
@@ -150,7 +152,7 @@ class StartPage(tk.Frame):
 
         filename = tk.filedialog.askopenfilename(filetypes=[('TXT','*.txt'), ('CSV','*.csv')], title='Select VECTORNAV file')
 
-        na_vals = ['', '#NA', 'N/A', '-nan', 'nan', 'NAN', 'NaN', 'NULL', 'null', ' ']
+        print('Reading INS file...')
         self.vn_raw = pd.read_table(filename, skiprows=[1], sep='\t', converters={0:str, 1:lambda x: float(x or 0)})
         #dropping excess columns first
 
@@ -159,113 +161,110 @@ class StartPage(tk.Frame):
             splitted = column.split(' (')
 
             self.vn_raw.rename({column:splitted[0]}, axis='columns', inplace=True)
-            
-        #cleanup the mess that comes from VectorNav
-        self.vn_raw.drop('Dcm00', 1, inplace=True)
-        self.vn_raw.drop('Dcm01', 1, inplace=True)
-        self.vn_raw.drop('Dcm02', 1, inplace=True)
-        self.vn_raw.drop('Dcm10', 1, inplace=True)
-        self.vn_raw.drop('Dcm11', 1, inplace=True)
-        self.vn_raw.drop('Dcm12', 1, inplace=True)
-        self.vn_raw.drop('Dcm20', 1, inplace=True)
-        self.vn_raw.drop('Dcm21', 1, inplace=True)
-        self.vn_raw.drop('Dcm22', 1, inplace=True)
-        self.vn_raw.drop('YawPitchRollUncertainty.X', 1, inplace=True)
-        self.vn_raw.drop('YawPitchRollUncertainty.Y', 1, inplace=True)
-        self.vn_raw.drop('YawPitchRollUncertainty.Z', 1, inplace=True)
-        self.vn_raw.drop('UncompensatedAcceleration.X', 1, inplace=True)
-        self.vn_raw.drop('UncompensatedAcceleration.Y', 1, inplace=True)
-        self.vn_raw.drop('UncompensatedAcceleration.Z', 1, inplace=True)
-        self.vn_raw.drop('UncompensatedMagnetic.X', 1, inplace=True)
-        self.vn_raw.drop('UncompensatedMagnetic.Y', 1, inplace=True)
-        self.vn_raw.drop('UncompensatedMagnetic.Z', 1, inplace=True)
-        self.vn_raw.drop('UncompensatedAngularRate.X', 1, inplace=True)
-        self.vn_raw.drop('UncompensatedAngularRate.Y', 1, inplace=True)
-        self.vn_raw.drop('UncompensatedAngularRate.Z', 1, inplace=True)
-        self.vn_raw.drop('EstimatedPositionEcef.X', 1, inplace=True)
-        self.vn_raw.drop('EstimatedPositionEcef.Y', 1, inplace=True)
-        self.vn_raw.drop('EstimatedPositionEcef.Z', 1, inplace=True)
-        self.vn_raw.drop('EstimatedVelocityBody.X', 1, inplace=True)
-        self.vn_raw.drop('EstimatedVelocityBody.Y', 1, inplace=True)
-        self.vn_raw.drop('EstimatedVelocityBody.Z', 1, inplace=True)
-        self.vn_raw.drop('EstimatedVelocityEcef.X', 1, inplace=True)
-        self.vn_raw.drop('EstimatedVelocityEcef.Y', 1, inplace=True)
-        self.vn_raw.drop('EstimatedVelocityEcef.Z', 1, inplace=True)
-        self.vn_raw.drop('EstimatedPositionUncertainty', 1, inplace=True)
-        self.vn_raw.drop('EstimatedVelocityUncertainty', 1, inplace=True)
-        self.vn_raw.drop('GpsTow', 1, inplace=True)
-        self.vn_raw.drop('GpsTowNs', 1, inplace=True)
-        self.vn_raw.drop('GpsWeek', 1, inplace=True)
-        self.vn_raw.drop('GpsPositionEcef.X', 1, inplace=True)
-        self.vn_raw.drop('GpsPositionEcef.Y', 1, inplace=True)
-        self.vn_raw.drop('GpsPosition.Z', 1, inplace=True)
 
-        self.vn_raw.drop('GpsVelocityEcef.X', 1, inplace=True)
-        self.vn_raw.drop('GpsVelocityEcef.Y', 1, inplace=True)
-        self.vn_raw.drop('GpsVelocityEcef.Z', 1, inplace=True)
-        self.vn_raw.drop('GpsTimeUncertainty', 1, inplace=True)
-        self.vn_raw.drop('ImuStatus', 1, inplace=True)
-        self.vn_raw.drop('VpeStatus', 1, inplace=True)
-        self.vn_raw.drop('TimeSyncIn', 1, inplace=True)
-        self.vn_raw.drop('SyncInCount', 1, inplace=True)
-        self.vn_raw.drop('DeltaTime', 1, inplace=True)
-        self.vn_raw.drop('DeltaTheta.X', 1, inplace=True)
-        self.vn_raw.drop('DeltaTheta.Y', 1, inplace=True)
-        self.vn_raw.drop('DeltaTheta.Z', 1, inplace=True)
-        self.vn_raw.drop('DeltaVelocity.X', 1, inplace=True)
-        self.vn_raw.drop('DeltaVelocity.Y', 1, inplace=True)
-        self.vn_raw.drop('DeltaVelocity.Z', 1, inplace=True)
-        self.vn_raw.drop('TimeGpsPps', 1, inplace=True)
-        self.vn_raw.drop('TimeUtc', 1, inplace=True)
+        rows_to_remove = ['Dcm00', 
+        'Dcm01', 
+        'Dcm02', 
+        'Dcm10', 
+        'Dcm11', 
+        'Dcm12', 
+        'Dcm20', 
+        'Dcm21', 
+        'Dcm22', 
+        'YawPitchRollUncertainty.X', 
+        'YawPitchRollUncertainty.Y', 
+        'YawPitchRollUncertainty.Z', 
+        'UncompensatedAcceleration.X', 
+        'UncompensatedAcceleration.Y', 
+        'UncompensatedAcceleration.Z', 
+        'UncompensatedMagnetic.X', 
+        'UncompensatedMagnetic.Y', 
+        'UncompensatedMagnetic.Z', 
+        'UncompensatedAngularRate.X', 
+        'UncompensatedAngularRate.Y', 
+        'UncompensatedAngularRate.Z', 
+        'EstimatedPositionEcef.X', 
+        'EstimatedPositionEcef.Y', 
+        'EstimatedPositionEcef.Z', 
+        'EstimatedVelocityBody.X', 
+        'EstimatedVelocityBody.Y', 
+        'EstimatedVelocityBody.Z', 
+        'EstimatedVelocityEcef.X', 
+        'EstimatedVelocityEcef.Y', 
+        'EstimatedVelocityEcef.Z', 
+        'EstimatedPositionUncertainty', 
+        'EstimatedVelocityUncertainty', 
+        'GpsTow', 
+        'GpsTowNs', 
+        'GpsWeek', 
+        'GpsPositionEcef.X', 
+        'GpsPositionEcef.Y', 
+        'GpsPosition.Z', 
+        'GpsVelocityEcef.X', 
+        'GpsVelocityEcef.Y', 
+        'GpsVelocityEcef.Z', 
+        'GpsTimeUncertainty', 
+        'ImuStatus', 
+        'VpeStatus', 
+        'TimeSyncIn', 
+        'SyncInCount', 
+        'DeltaTime', 
+        'DeltaTheta.X', 
+        'DeltaTheta.Y', 
+        'DeltaTheta.Z', 
+        'DeltaVelocity.X', 
+        'DeltaVelocity.Y', 
+        'DeltaVelocity.Z', 
+        'TimeGpsPps', 
+        'TimeUtc', 
+        'LinearAccelerationBody.X', 
+        'LinearAccelerationBody.Y', 
+        'LinearAccelerationBody.Z', 
+        'LinearAccelerationNed.X', 
+        'LinearAccelerationNed.Y', 
+        'LinearAccelerationNed.Z', 
+        'AccelerationEcef.X', 
+        'AccelerationEcef.Y', 
+        'AccelerationEcef.Z', 
+        'LinearAccelerationEcef.X', 
+        'LinearAccelerationEcef.Y', 
+        'LinearAccelerationEcef.Z', 
+        'MagneticNed.X', 
+        'MagneticNed.Y', 
+        'MagneticNed.Z', 
+        'MagneticEcef.X', 
+        'MagneticEcef.Y', 
+        'MagneticEcef.Z', 
+        'GpsPositionAccuracyEcef.X', 
+        'GpsPositionAccuracyEcef.Y', 
+        'GpsPositionAccuracyEcef.Z', 
+        'EstimatedAttitudeUncertainty', 
+        'Quaternion.X', 
+        'Quaternion.Y', 
+        'Quaternion.Z', 
+        'Quaternion.W', 
+        'EstimatedPositionLla.Latitude', 
+        'EstimatedPositionLla.Longitude', 
+        'EstimatedPositionLla.Altitude', 
+        'GpsTimestampLocalTime', 
+        'GpsPositionUncertainty.X', 
+        'GpsPositionUncertainty.Y', 
+        'GpsPositionUncertainty.Z', 
+        'GpsVelocityUncertainty', 
+        'SensorSaturation']
 
-        self.vn_raw.drop('LinearAccelerationBody.X', 1, inplace=True)
-        self.vn_raw.drop('LinearAccelerationBody.Y', 1, inplace=True)
-        self.vn_raw.drop('LinearAccelerationBody.Z', 1, inplace=True)
-        self.vn_raw.drop('LinearAccelerationNed.X', 1, inplace=True)
-        self.vn_raw.drop('LinearAccelerationNed.Y', 1, inplace=True)
-        self.vn_raw.drop('LinearAccelerationNed.Z', 1, inplace=True)
-        self.vn_raw.drop('AccelerationEcef.X', 1, inplace=True)
-        self.vn_raw.drop('AccelerationEcef.Y', 1, inplace=True)
-        self.vn_raw.drop('AccelerationEcef.Z', 1, inplace=True)
-        self.vn_raw.drop('LinearAccelerationEcef.X', 1, inplace=True)
-        self.vn_raw.drop('LinearAccelerationEcef.Y', 1, inplace=True)
-        self.vn_raw.drop('LinearAccelerationEcef.Z', 1, inplace=True)
-        self.vn_raw.drop('MagneticNed.X', 1, inplace=True)
-        self.vn_raw.drop('MagneticNed.Y', 1, inplace=True)
-        self.vn_raw.drop('MagneticNed.Z', 1, inplace=True)
-        self.vn_raw.drop('MagneticEcef.X', 1, inplace=True)
-        self.vn_raw.drop('MagneticEcef.Y', 1, inplace=True)
-        self.vn_raw.drop('MagneticEcef.Z', 1, inplace=True)
-        self.vn_raw.drop('GpsPositionAccuracyEcef.X', 1, inplace=True)
-        self.vn_raw.drop('GpsPositionAccuracyEcef.Y', 1, inplace=True)
-        self.vn_raw.drop('GpsPositionAccuracyEcef.Z', 1, inplace=True)
-        self.vn_raw.drop('EstimatedAttitudeUncertainty', 1, inplace=True)
-        self.vn_raw.drop('Quaternion.X', 1, inplace=True)
-        self.vn_raw.drop('Quaternion.Y', 1, inplace=True)
-        self.vn_raw.drop('Quaternion.Z', 1, inplace=True)
-        self.vn_raw.drop('Quaternion.W', 1, inplace=True)
-
-        self.vn_raw.drop('EstimatedPositionLla.Latitude', 1, inplace=True)
-        self.vn_raw.drop('EstimatedPositionLla.Longitude', 1, inplace=True)
-        self.vn_raw.drop('EstimatedPositionLla.Altitude', 1, inplace=True)
-
-        self.vn_raw.drop('GpsTimestampLocalTime', 1, inplace=True)
-        self.vn_raw.drop('GpsPositionUncertainty.X', 1, inplace=True)
-        self.vn_raw.drop('GpsPositionUncertainty.Y', 1, inplace=True)
-        self.vn_raw.drop('GpsPositionUncertainty.Z', 1, inplace=True)
-        self.vn_raw.drop('GpsVelocityUncertainty', 1, inplace=True)
-
-        self.vn_raw.drop('SensorSaturation', 1, inplace=True)
+        for row in rows_to_remove:
+            self.vn_raw.drop(row, 1, inplace=True)
 
 
         # since the dataset is duplicated (I do not know why), I am getting rid of duplicates by direct index
         self.vn_clean = self.vn_raw.iloc[:, [j for j, c in enumerate(self.vn_raw.columns) if j < 37]].copy()
+
+        # padding the file to fill voids before merging
         self.vn_clean.fillna(method='pad', inplace=True)
 
         #vectornav timestamp
         self.vn_clean['time'] = pd.to_datetime(self.vn_clean['Timestamp'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
-        vn_dt = self.vn_clean['time'][1]-self.vn_clean['time'][0]
-        #print('the vn delta t is ', vn_dt, type(vn_dt))
 
         #calculate the differential of the z accel signal
         # for peak detection
@@ -279,46 +278,56 @@ class StartPage(tk.Frame):
         '''this function will detect the peaks and return the timedelta
         '''
         peaks_not_detected = True
-        while peaks_not_detected:
+        user_cancelled = False
+
+        while (peaks_not_detected and not user_cancelled):
             print()
             print()
             print('input GRAPHTEC peak threshold')
             graphtec_mph = tksd.askfloat("Input", "Enter GRAPHTEC peak threshold", minvalue = 0.0001, maxvalue = 5.0)
-            print()
-            print()
-            print('input VectorNav peak threshold')
-            #get the threshold
-            vn_mph = tksd.askfloat("Input", "Enter VectorNav peak threshold", minvalue = 0.0001, maxvalue = 50.0)
-   
+            if graphtec_mph:
 
-            peaks = detect_peaks(self.graphtec['CH8_diff'].values, mph=graphtec_mph)          
+                print()
+                print()
+                print('input VectorNav peak threshold')
+                #get the threshold
+                vn_mph = tksd.askfloat("Input", "Enter VectorNav peak threshold", minvalue = 0.0001, maxvalue = 50.0)
+                if vn_mph:
+                    peaks = detect_peaks(self.graphtec['CH8_diff'].values, mph=graphtec_mph)          
 
-            peaks2 = detect_peaks(self.vn_clean['Acceleration.Z_diff'].values, mph=vn_mph)
-            if len(peaks)==0:
-                print('Graphtec peaks not detectd - choose a lower threshold')
-            if len(peaks2)==0:
-                print('VectorNav peaks not detectd - choose a lower threshold')
-            if (len(peaks)>0) and (len(peaks2)>0):
-                print('peaks found')
-                peaks_not_detected = False
+                    peaks2 = detect_peaks(self.vn_clean['Acceleration.Z_diff'].values, mph=vn_mph)
+                    if len(peaks)==0:
+                        print('Graphtec peaks not detectd - choose a lower threshold')
+                    if len(peaks2)==0:
+                        print('VectorNav peaks not detectd - choose a lower threshold')
+                    if (len(peaks)>0) and (len(peaks2)>0):
+                        print('peaks found')
+                        peaks_not_detected = False
+                else:
+                    user_cancelled = True
+            else:
+                user_cancelled = True
+        if user_cancelled == False:
             
-        print('Graphtec initial timestamp: {}'.format(self.graphtec['wrong_dt'].iloc[0]))
-        print('Graphtec peak detected at: {}'.format(self.graphtec['wrong_dt'].iloc[peaks[0]]))
-        print('VectorNav initial timestamp: {}'.format(self.vn_clean['time'].iloc[0]))
-        print('VectorNav peak detected at: {}'.format(self.vn_clean['time'].iloc[peaks2[0]]))
-        print()
+            print('Graphtec initial timestamp: {}'.format(self.graphtec['wrong_dt'].iloc[0]))
+            print('Graphtec peak detected at: {}'.format(self.graphtec['wrong_dt'].iloc[peaks[0]]))
+            print('VectorNav initial timestamp: {}'.format(self.vn_clean['time'].iloc[0]))
+            print('VectorNav peak detected at: {}'.format(self.vn_clean['time'].iloc[peaks2[0]]))
+            print()
 
-        dt_window = 2 # plus or minus seconds around found peak
-        
-        delta_time = self.graphtec['wrong_dt'].iloc[peaks[0]]-self.vn_clean['time'].iloc[peaks2[0]]
-        print()
-        gr_plt_data = self.graphtec[(self.graphtec['wrong_dt'] > (self.graphtec['wrong_dt'].iloc[peaks[0]]-pd.Timedelta(seconds=2))) &
-              (self.graphtec['wrong_dt'] < (self.graphtec['wrong_dt'].iloc[peaks[0]]+pd.Timedelta(seconds=2)))]
+            dt_window = 2 # plus or minus seconds around found peak
+            
+            delta_time = self.graphtec['wrong_dt'].iloc[peaks[0]]-self.vn_clean['time'].iloc[peaks2[0]]
+            print()
+            gr_plt_data = self.graphtec[(self.graphtec['wrong_dt'] > (self.graphtec['wrong_dt'].iloc[peaks[0]]-pd.Timedelta(seconds=2))) &
+                  (self.graphtec['wrong_dt'] < (self.graphtec['wrong_dt'].iloc[peaks[0]]+pd.Timedelta(seconds=2)))]
 
-        vn_plt_data = self.vn_clean[(self.vn_clean['time'] > (self.vn_clean['time'].iloc[peaks2[0]]-pd.Timedelta(seconds=2))) &
-              (self.vn_clean['time'] < (self.vn_clean['time'].iloc[peaks2[0]]+pd.Timedelta(seconds=2)))]
+            vn_plt_data = self.vn_clean[(self.vn_clean['time'] > (self.vn_clean['time'].iloc[peaks2[0]]-pd.Timedelta(seconds=2))) &
+                  (self.vn_clean['time'] < (self.vn_clean['time'].iloc[peaks2[0]]+pd.Timedelta(seconds=2)))]
 
-        return peaks, peaks2, gr_plt_data, vn_plt_data, delta_time
+            return peaks, peaks2, gr_plt_data, vn_plt_data, delta_time
+        else:
+            return [np.array(None), np.array(None), np.array(None), np.array(None), np.array(None)]
 
 
 
@@ -332,13 +341,14 @@ class StartPage(tk.Frame):
         else:
 
             self.gr_peaks, self.vn_peaks, self.gr_data, self.vn_data, self.deltat = self.search_peaks()
+            if self.gr_peaks.all():
 
-            print('Delta is: {}'.format(self.deltat))
+                print('Delta is: {}'.format(self.deltat))
+                print('finished peaks routine')
+                
+                self.saveOK = True
+                self.updatePlot(self.gr_data, self.vn_data)
 
-            print('finished peaks routine')
-            
-            self.saveOK = True
-            self.updatePlot(self.gr_data, self.vn_data)
 
     def saveValues(self):
 
@@ -347,16 +357,11 @@ class StartPage(tk.Frame):
         else:
             
             new_time = self.wrong_dt - (self.graphtec['wrong_dt'].iloc[self.gr_peaks[0]]-self.vn_clean['time'].iloc[self.vn_peaks[0]])
-
-            print('now I am here 01')
-
             self.graphtec['time'] = new_time
 
             
             #join values
             result = self.vn_clean.append(self.graphtec.iloc[0:,:]).sort_values(by=['time'])
-
-
 
             #droping stuff we dont need
             ##result = result.drop('Number', 1)
@@ -395,46 +400,6 @@ class StartPage(tk.Frame):
         B1.pack()
         popup.mainloop()
  
-
-    
-
-class PageOne(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        
-        label = tk.Label(self, text="Select the Graphtec file for upload", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Page Two",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
-
-
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Page One",
-                            command=lambda: controller.show_frame(PageOne))
-        button2.pack()
-
-
-
-
         
 
 app = FTISyncApp()
